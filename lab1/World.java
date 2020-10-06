1f3c3a5f55c237f6e6b1a6670009bf6ee7b8c3e5
Kamil Delekta <kamildelekta@MacBook-Pro-Kamil.local>
2020-10-06 22:30:32 +0200

    Updating World.java again

--- lab1/World.java
+++ lab1/World.java
@@ -14,6 +14,7 @@ public class World {
             switch (args[i]){
                 case "f":
                     arr[i] = Direction.FORWARD;
+                    break;
                 case "b":
                     arr[i] = Direction.BACKWARD;
                     break;
